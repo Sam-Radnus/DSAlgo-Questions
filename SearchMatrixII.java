72495085d265c0acd5725c813f6de909f410e846
samsundar <samradnus2001@gmail.com>
2022-05-02 11:55:15 +0530

    Remove Digit to get Maximum Result

--- SearchMatrixII.java
+++ SearchMatrixII.java
@@ -22,8 +22,6 @@ public class SearchMatrixII {
     }
     public static void main(String args[])
     {
-        int ar[][]={{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-        printArray5(ar[0]);
 
     }
 }
