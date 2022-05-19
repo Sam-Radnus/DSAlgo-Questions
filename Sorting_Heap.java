0b02ef60f5de165512202d4e48925c78ba514e1e
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-05-19 20:08:17 +0530

    Heap Sort

---
+++ Sorting_Heap.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Sorting_Heap {
+    public static void sort(int ar[])
+    {
+        int n=ar.length;
+        for(int i=(n/2)-1;i>=0;i--)
+        {
+            heapify(ar,n,i);
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            //move current node to end
+            int temp=ar[0];
+            ar[0]=ar[i];
+            ar[i]=temp;
+            heapify(ar,i,0);
+        }
+        System.out.println(Arrays.toString(ar));
+    }
+    public static void heapify(int ar[],int n,int i)
+    {
+        int largest=i;
+        int l=2*i+1;
+        int r=2*i+2;
+
+        if(l<n && ar[largest]<ar[l]) {
+            largest=l;
+        }
+        if(r<n && ar[largest]<ar[r]) {
+            largest=r;
+        }
+
+        if(largest!=i)
+        {
+            int temp=ar[largest];    //placing the largest element at top
+            ar[largest]=ar[i];
+            ar[i]=temp;
+            heapify(ar,n,largest);
+        }
+
+    }
+    public static void main(String args[]){
+        sort(new int[]{5,4,3,2,1});
+    }
+}
