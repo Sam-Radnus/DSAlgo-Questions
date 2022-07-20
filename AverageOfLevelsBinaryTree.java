ec044f9eaaeaee46d406de0718f1eefcc36b9b9a
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-07-20 08:06:08 +0530

    Evaluate Boolean Binary Tree

--- AverageOfLevelsBinaryTree.java
+++ AverageOfLevelsBinaryTree.java
@@ -38,6 +38,21 @@ public class AverageOfLevelsBinaryTree {
            }
            return res;
 
+        }
+        //Evaluate Tree
+    public static boolean evaluateTree(TreeNode root) {
+        if(root.val==0||root.val==1)
+            return root.val==1;
+        return helper(root);
+    }
+    public static boolean helper(TreeNode root)
+    {
+        if(root.val==2)
+            return helper(root.left)||helper(root.right);
+        if(root.val==3)
+            return helper(root.left)&&helper(root.right);
+        return root.val==1;
+
     }
     public static void main(String args[])
       {
