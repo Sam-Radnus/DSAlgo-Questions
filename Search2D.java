8cc118baa34d98b31f8762db41d853b9fafffa2a
Sam-Sundar <68651028+Sam-Radnus@users.noreply.github.com>
2022-12-26 10:38:20 +0530

    Optimized Solution

--- Search2D.java
+++ Search2D.java
@@ -1,54 +1,20 @@
-public class Search2D{
-    public boolean binary_search(int[]matrix,int target,int start,int end)
-    {
-        int mid=0;
-        while(start<=end)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0||matrix[0].length==0)
         {
-            mid=start+(end-start)/2;
-            if(matrix[mid]==target)
-            {
-                return true;
-            }
-            else{
-                if(matrix[mid]<target)
-                {
-                    start=mid+1;
-                }
-                else
-                {
-                    end=mid-1;
-                }
-            }
+            return false;
+        }
+        int row=0;
+        int col=matrix[0].length-1;
+        while(row<matrix.length && col>=0)
+        {
+            if(matrix[row][col]==target)
+               return true;
+            if(matrix[row][col]<target)
+               row++;
+            else
+               col--;     
         }
         return false;
-
-    }
-    public boolean searchMatrix(int[][] matrix, int target) {
-        boolean found=false;
-        int i,j,end;
-        for(i=0;i<matrix.length;i++)
-        {
-            for(j=0;j<matrix[0].length;j++)
-            {
-                end=matrix[0].length-1;
-                if(matrix[i][end] < target)
-                {
-                    break;
-                }
-                else
-                {
-                    found=binary_search(matrix[i],target,0,end+1);
-
-                }
-
-            }
-            if(found)
-            { break;}
-        }
-        return found;
-    }
-    public static void main(String args[])
-    {
-
     }
 }
