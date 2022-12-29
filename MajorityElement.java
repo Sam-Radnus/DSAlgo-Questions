38cca22e2cd1b84d2cf6dfab51a467b3d9cea82b
Sam-Sundar <68651028+Sam-Radnus@users.noreply.github.com>
2022-12-29 11:05:36 +0530

    169. Majority Element

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,41 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class MajorityElement {
-    public static Map<Integer,Integer> countNums(int[]nums)
-    {
-        int c=0;
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority=0;
+        int count=0;
+        for(int num:nums)
         {
-            if(!map.containsKey(nums[i]))
+            if(count==0)
             {
-                c=1;
-                map.put(nums[i],c);
+                majority=num;
             }
-            else
+            if(num==majority)
             {
-                map.put(nums[i],map.get(nums[i])+1);
+                count++;
+            }
+            else{
+                count--;
             }
         }
-        return map;
-    }
-    public static int majorityElement(int[] nums) {
-
-        Map<Integer,Integer> counts=countNums(nums);
-        Map.Entry<Integer,Integer>majorityEntry=null;
-        for(Map.Entry<Integer,Integer>entry: counts.entrySet())
-        {
-            if(majorityEntry==null||entry.getValue()>majorityEntry.getValue())
-            {
-                majorityEntry=entry;
-            }
-        }
-        return majorityEntry.getKey();
-    }
-    public static void main(String args[])
-    {
-        int nums[]={2,2,1,1,1,2,2};
-        System.out.println(majorityElement(nums));
+        return majority;
     }
 }
