9e4ef86a78ed175be6f8a0b6f7f47da475928f39
Sam-Sundar <68651028+Sam-Radnus@users.noreply.github.com>
2022-12-29 18:30:48 +0530

    76. Minimum Window Substring

---
+++ MinimumWindowSubstring.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        Map<Character,Integer>map=new HashMap<>();
+        if(s == null || s.length() < t.length() || s.length() == 0){
+        return "";
+    }
+       for(char c : t.toCharArray()){
+        if(map.containsKey(c)){
+            map.put(c,map.get(c)+1);
+        }else{
+            map.put(c,1);
+        }
+    }
+        int minLeft=0;
+        int minLen=s.length()+1;
+        int count=0;
+        int right=0,left=0;
+        for(right=0;right<s.length();right++)
+        {
+            if(map.containsKey(s.charAt(right)))
+            {
+                map.put(s.charAt(right),map.get(s.charAt(right))-1);
+                if(map.get(s.charAt(right))>=0) 
+                {
+                    count++;
+                }
+            
+            while(count==t.length())
+            {
+                if(right-left+1<minLen)
+                {
+                    minLeft=left;
+                    minLen=right-left+1;
+                }
+                if(map.containsKey(s.charAt(left)))
+                {
+                    map.put(s.charAt(left),map.get(s.charAt(left))+1);
+                    if(map.get(s.charAt(left))>0)
+                    {
+                       count--;
+                    }
+                }
+                left++;
+            }
+            }
+        }
+        if(minLen>s.length())
+        {
+            return "";
+        }
+        return s.substring(minLeft,minLeft+minLen);
+    }
+}
