fc6f5d3b0308f796bd59fdd3229d14ce46ef7c66
Sam-Sundar <68651028+Sam-Radnus@users.noreply.github.com>
2023-12-02 13:41:35 +0530

    Hard

---
+++ LongestValidParenthesis.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack=[-1]
+        length=0
+        for i in range(len(s)):
+            if s[i]=='(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    length=max(length,i-stack[-1])        
+        return length
+            
+        
